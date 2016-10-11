bc0997b92782731e02231ef308b6986568f77280
joshua <sergej.ko.13.13@gmail.com>
2016-10-11 20:49:09 +0300

    11 october start3

--- tmp2.js
+++ tmp2.js
@@ -20,15 +20,43 @@
 //console.log(nextId2());
 //console.log(nextId1());
 //console.log(nextId2());
+//
+//
+//function creatGreeting(greeting) {
+//	return function (name) {
+//		console.log(`${greeting}, ${name}`);
+//	}
+//}
+//var sayHi = creatGreeting(`hi`);
+//sayHi(`bob`);
+//var sayHello = creatGreeting(`Hello`);
+//sayHello(`Anna`);
+//
 
-
-function creatGreeting(greeting) {
-	return function (name) {
-		console.log(`${greeting}, ${name}`);
-	}
+//var allFriends = [`Mike`, `Spike`, `Trike`, `Bike`];
+//
+//function greetFriends(friends, greet) {
+//	for (var i = 0; i < friends.length; i++) {
+//		greet(friends[i]);
+//
+//	}
+//}
+////
+////function greet(friend) {
+////	console.log(`Hello, ` + friend);
+////}
+//greetFriends(allFriends, function (friend) {
+//	console.log(`Hi, ` + friend);
+//});
+function genericPoemMaker(name, gender) {
+	console.log(name + ` finder`);
+	console.log(`altruistic`);
+	console.log(`style`);
+	console.log(`A ` + gender + ` smile`);
 }
-var sayHi = creatGreeting(`hi`);
-sayHi(`bob`);
-var sayHello = creatGreeting(`Hello`);
-sayHello(`Anna`);
-console.log(sayHi);
+
+function genericUserInput(firstName, lastName, gender, callback) {
+	var name = firstName + " " + lastName;
+	callback(name, gender);
+}
+var say = genericUserInput("Anna", "Zii", "GIRL", genericPoemMaker);
